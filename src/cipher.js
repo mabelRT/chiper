f519953fe9489789240c2b44822325ec758cf9a1
mabel <wendy.torrico.wt@gmail.com>
2021-09-15 10:31:33 -0300

    mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
+
+//cifrar//
+    encode (string,offset) {
+          
+      let resultado = "";
+      const cadena =  string.split("");
+      const letras=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";   //cadena para las letras//
+      offset = (offset % 26 + 26) % 26; //doble modulo para el desplazamiento//
+     
+      if(string){ //para asegurarnos que lo que se ingrese sean solo letras//
+          for(let i =0; i<cadena.length; i++){
+               
+            if (letras.indexOf(cadena[i])!== -1){
+                let posicion = ((letras.indexOf(cadena[i])+offset)%26);
+                resultado += letras[posicion]; 
+            }
+            else {
+                resultado += string[i];
+            }
+          }
+          return resultado;
+      }
+  },
+//descifrar//
+    decode (string, offset) {
+              if (!string)
+              return"";
+               const letras= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+               offset= (offset % 26 - 26) % 26;
+               return string.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-offset) %26]);
+            }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import cipher from './cipher.js';
-console.log(cipher);
+//convertir las leras en mayusculas//
+document.getElementById("message").addEventListener("keyup", function name() {
+    this.value = this.value.toUpperCase();
+}, true);
+//recoger datos para cifrar//
+
+document.getElementById("encode").addEventListener("click", function valores() {
+    const string = document.getElementById("message").value;
+    const offset = document.getElementById("offset").value;
+    document.getElementById("message2").value = cipher.encode(string,offset);
+}, true);  
+
+//recoger datos para descifrar//
+document.getElementById("decode").addEventListener("click",function valores2() {
+    const string = document.getElementById("message2").value;
+    const offset = document.getElementById("offset").value;
+    document.getElementById("message2").value = cipher.decode(string,offset);
+}, true);
+
+
+
